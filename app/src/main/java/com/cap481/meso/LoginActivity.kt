bf6acbcb78ef077d817b76ae67368f168dbc096a
khlanwar <khairulanw33@gmail.com>
2021-05-30 15:05:36 +0700

    Create an activity login

--- app/src/main/java/com/cap481/meso/LoginActivity.kt
+++ app/src/main/java/com/cap481/meso/LoginActivity.kt
@@ -1,11 +1,104 @@
 package com.cap481.meso
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
+import androidx.core.content.ContextCompat
+import com.cap481.meso.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private var emailValid = false
+    private var passwordConfirmationValid = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.tvNotHaveAccount.setOnClickListener{
+            intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener{
+            intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        validateButton()
+
+        binding.edEmail.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                validateEmail()
+            }
+        })
+
+        binding.edConfirmPassword.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                validatePasswordConfirmation()
+            }
+        })
+
+
+    }
+
+    fun validateEmail() {
+        val input = binding.edEmail.text.toString()
+        if (!Patterns.EMAIL_ADDRESS.matcher(input).matches()) {
+            emailValid = false
+            showEmailExistAlert(true)
+        } else {
+            emailValid = true
+            showEmailExistAlert(false)
+        }
+        validateButton()
+    }
+
+    fun validatePasswordConfirmation() {
+        val input = binding.edConfirmPassword.text.toString()
+        if (input.length < 6) {
+            passwordConfirmationValid = false
+            showPasswordConfirmationAlert(true)
+        } else {
+            passwordConfirmationValid = true
+            showPasswordConfirmationAlert(false)
+        }
+        validateButton()
+    }
+
+    private fun validateButton() {
+        if ( emailValid && passwordConfirmationValid) {
+            binding.btnLogin.isEnabled = true
+            binding.btnLogin.setBackgroundColor(ContextCompat.getColor(this, R.color.color_background_green))
+        } else {
+            binding.btnLogin.isEnabled = false
+            binding.btnLogin.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+        }
+    }
+
+    private fun showEmailExistAlert(isNotValid: Boolean) {
+        binding.edEmail.error = if (isNotValid) getString(R.string.email_not_valid) else null
+    }
+
+    private fun showPasswordConfirmationAlert(isNotValid: Boolean) {
+        binding.edConfirmPassword.error = if (isNotValid) getString(R.string.password_not_valid) else null
     }
 }
