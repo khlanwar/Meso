4d1b145c4d2f2f1cd1abd4b739e5d3d26d02214a
khlanwar <khairulanw33@gmail.com>
2021-05-31 22:14:13 +0700

    Add firebase cloud database and storage

--- app/src/main/java/com/cap481/meso/ui/ProfileFragment.kt
+++ app/src/main/java/com/cap481/meso/ui/ProfileFragment.kt
@@ -1,60 +1,123 @@
 package com.cap481.meso.ui
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.cap481.meso.R
+import android.widget.Toast
+import com.cap481.meso.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
+import java.io.ByteArrayOutputStream
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var imageUri: Uri
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+
+        val user = auth.currentUser
+
+        if (user != null){
+            if (user.photoUrl != null){
+                Picasso.get().load(user.photoUrl).into(binding.ivProfile)
+            }else{
+                Picasso.get().load("https://picsum.photos/id/1/200/300").into(binding.ivProfile)
+            }
+            binding.etName.setText(user.displayName)
+            binding.etEmail.setText(user.email)
+        }
+
+        binding.ivProfile.setOnClickListener{
+            intentCamera()
+        }
+
+        binding.btnUpdate.setOnClickListener{
+            val image = when{
+                ::imageUri.isInitialized -> imageUri
+                user?.photoUrl == null -> Uri.parse("https://picsum.photos/id/1/200/300")
+                else -> user.photoUrl
+            }
+            val name = binding.etName.text.toString().trim()
+            if (name.isEmpty()){
+                binding.etName.error = "Input your name"
+                binding.etName.requestFocus()
+                return@setOnClickListener
+            }
+
+            UserProfileChangeRequest.Builder()
+                .setDisplayName(name)
+                .setPhotoUri(image)
+                .build().also {
+                    user?.updateProfile(it)?.addOnCompleteListener{
+                        if (it.isSuccessful){
+                            Toast.makeText(activity,"Profile Updated",Toast.LENGTH_SHORT).show()
+                        } else{
+                            Toast.makeText(activity,"${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    private fun intentCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+            activity?.packageManager?.let {
+                intent.resolveActivity(it).also {
+                    startActivityForResult(intent, REQUEST_CAMERA)
+                }
+            }
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK){
+            val imgBitmap = data?.extras?.get("data") as Bitmap
+            uploadImage(imgBitmap)
+        }
+    }
+
+    private fun uploadImage(imgBitmap: Bitmap) {
+        val baos = ByteArrayOutputStream()
+        val ref = FirebaseStorage.getInstance().reference.child("img/${FirebaseAuth.getInstance().currentUser?.uid}")
+        imgBitmap.compress(Bitmap.CompressFormat.JPEG,100,baos)
+        val image = baos.toByteArray()
+        ref.putBytes(image)
+            .addOnCompleteListener{
+                if (it.isSuccessful){
+                    ref.downloadUrl.addOnCompleteListener{
+                        it.result?.let {
+                            imageUri = it
+                            binding.ivProfile.setImageBitmap(imgBitmap)
+                        }
+                    }
                 }
             }
     }
+
+    companion object{
+        const val REQUEST_CAMERA = 100
+    }
+
 }
